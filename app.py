7f9b143b9d3d4ee3bbdb1561bf5ecbfeb24ddbf7
Diana <124718372+DianaCioroiu2001@users.noreply.github.com>
2026-09-11 21:20:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import subprocess
 # CONFIGURARE BUNNY, WORDPRESS & CAPITOLE
 # ==========================================
 
-BUNNY_API_KEY="f2ad32f2-ba5b-4e31-ae46-51782e8536d59644de27-9601-4147-b209-1c53c7259464"
+BUNNY_STREAM_API_KEY="f2ad32f2-ba5b-4e31-ae46-51782e8536d59644de27-9601-4147-b209-1c53c7259464"
 
 BUNNY_LIBRARIES = {
     "Grupa 5A": {
